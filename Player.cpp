4ef5df1e1085bd224b20274b301fb1ce62ecc9b6
Ali Azadi <aliazadicode@gmail.com>
2024-07-02 18:03:30 +0330

    Made the handleRightMovement and handleKeyRelease methods

--- Player.cpp
+++ Player.cpp
@@ -7,19 +7,35 @@
 
 
 
-Player::Player(int Width, int Height, Position _position, QGraphicsPixmapItem* Image, int Speed, Position Velocity) :
-    BodyObject(Width, Height, _position, Image)
+Player::Player(int Width, int Height, Position _position, QGraphicsPixmapItem* Image, int Speed, Position Velocity, QGraphicsScene* _scene) :
+    BodyObject(Width, Height, _position, Image), imageIndex(0), scene(_scene)
 {
     speed = Speed;
     velocity = Velocity;
+
+    runningImages.push_back(new QGraphicsPixmapItem(QPixmap(":/new/prefix1/img/Player_standing.png").scaled(100, 100)));
+    runningImages.push_back(new QGraphicsPixmapItem(QPixmap(":/new/prefix1/img/running1.png").scaled(100, 100)));
+    runningImages.push_back(new QGraphicsPixmapItem(QPixmap(":/new/prefix1/img/running2.png").scaled(100, 100)));
+    runningImages.push_back(new QGraphicsPixmapItem(QPixmap(":/new/prefix1/img/running3.png").scaled(100, 100)));
+    runningImages.push_back(new QGraphicsPixmapItem(QPixmap(":/new/prefix1/img/running4.png").scaled(100, 100)));
+    runningImages.push_back(new QGraphicsPixmapItem(QPixmap(":/new/prefix1/img/running5.png").scaled(100, 100)));
+
+
+
+
+
+
+
+
 }
 
 void Player::draw(QGraphicsScene &scene) {
     if (image){
-        image->setPixmap(image->pixmap().scaled(width, height,Qt::KeepAspectRatioByExpanding));
-        image->setPos(position.getX(), position.getY());
-        scene.addItem(image);
+        scene.removeItem(image);
     }
+    image = runningImages[imageIndex];
+    image->setPos(position.getX(), position.getY());
+    scene.addItem(image);
 }
 
 void Player::handleMovement(QKeyEvent* event){
@@ -27,7 +43,7 @@ void Player::handleMovement(QKeyEvent* event){
         // handleLeftMovement();
         std::cout<<"Left\n";
     } else if (event->key() == Qt::Key_Right) {
-        // handleRightMovement();
+        handleRightMovement();
         std::cout<<"Right\n";
     } else if (event->key() == Qt::Key_Up) {
         // handleUpMovement();
@@ -38,7 +54,19 @@ void Player::handleMovement(QKeyEvent* event){
     }
 }
 
+void Player::handleKeyRelease(QKeyEvent* event) {
+    if (event->key() == Qt::Key_Right || event->key() == Qt::Key_Left) {
+        imageIndex = 0; // Set to standing image
+        this->draw(*scene);
+    }
+}
 
-// void Player::handleRightMovement(){
 
-// }
+void Player::handleRightMovement() {
+    position.setX(position.getX() + 10);
+    position.setY(510);
+    imageIndex = (imageIndex + 1) % runningImages.size(); // Cycle through the running images
+    width = 100;
+    height = 100;
+    this->draw(*scene);
+}
--- Player.h
+++ Player.h
@@ -4,6 +4,7 @@
 #include "position.h"
 #include "bodyobject.h"
 #include <QKeyEvent>
+#include <vector>
 
 
 class Player : public BodyObject
@@ -11,8 +12,11 @@ class Player : public BodyObject
 private:
     int speed;
     Position velocity;
+    int imageIndex;
+    std::vector<QGraphicsPixmapItem*> runningImages;
+    QGraphicsScene* scene;
 public:
-    Player(int Width, int Height, Position _position, QGraphicsPixmapItem* Image, int Speed, Position Velocity);
+    Player(int Width, int Height, Position _position, QGraphicsPixmapItem* Image, int Speed, Position Velocity, QGraphicsScene* scene);
     void draw(QGraphicsScene &scene) override;
     void handleGravity();
     void handleRightMovement();
@@ -20,6 +24,7 @@ public:
     void handleUpMovement();
     void handleDownMovement();
     void handleMovement(QKeyEvent* event);
+    void handleKeyRelease(QKeyEvent* event);
 
 };
 
